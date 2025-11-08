841535d25d3b3fc816be1d457ec5b2b267a0578e
parsa <parsafarashah@gmail.com>
2025-11-08 16:03:49 +0330

    feat: add Pwa

--- src/js/index.js
+++ src/js/index.js
@@ -1495,3 +1495,15 @@ $searchBtn.addEventListener("click", () => {
     $search.focus();
   }
 });
+
+// ///////// for pwa
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("serviceWorker.js")
+    .then((reg) => {
+      console.log("Service worker registred successfully", reg);
+    })
+    .catch((err) => {
+      console.log("service worker not registred !!", err);
+    });
+}
